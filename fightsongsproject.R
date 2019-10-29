482bde0728c08d1ab410a8f216f1b07cc77c0898
anneskudera <56929084+anneskudera@users.noreply.github.com>
2019-10-29 13:41:34 -0400

    Update fightsongsproject.R

--- fightsongsproject.R
+++ fightsongsproject.R
@@ -3,3 +3,26 @@ View(songs)
 library(tidyverse)
 library(dplyr)
 
+# Histogram of bpm
+ggplot(data = songs,
+       mapping = aes(x = bpm)) +
+  geom_histogram(fill = 'orange', color = 'purple')
+# Numerical summary of bpm
+summary(songs$bpm)
+f %>%
+  summarise(bpmavg = mean(bpm),
+            bpmmed = median(bpm),
+            bpmstd = sd(bpm),
+            numsongs = n())
+
+# Boxplot of bpm by conference
+ggplot(data = songs,
+       mapping = aes(x = conference, y = bpm, fill = conference)) +
+  geom_boxplot()
+# Numerical summary of bpm by conference
+songs %>%
+  group_by(conference) %>%
+  summarise(bpmavg = mean(bpm),
+            bpmmed = median(bpm),
+            bpmstd = sd(bpm),
+            numsongs = n())
